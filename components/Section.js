20974ec343315c945abac2b71c0dea795d0a228e
jpbehler <juampi.gnr@gmail.com>
2023-05-18 19:26:05 -0300

    Fixing justification

--- components/Section.js
+++ components/Section.js
@@ -85,8 +85,8 @@ export default function Section(props){
   
 
     return(
-        <Grid.Container id={sectionID} className={`${css.section} observed`} gap={1} css={{padding:"5vw",...filling}}> 
-            {childNodes.map(child=> <Grid justify="center" {...deviceGrid[showUpTo]}>{child}</Grid>)} 
+        <Grid.Container id={sectionID} className={`${css.section} observed`} gap={2} css={{padding:"5vw",...filling}}> 
+            {childNodes.map(child=> <Grid justify="space-evenly" {...deviceGrid[showUpTo]}>{child}</Grid>)} 
         </Grid.Container> 
         );
 }

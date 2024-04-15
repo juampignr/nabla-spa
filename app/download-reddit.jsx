0c9c1a2fd8c97debd3c6072a1398d08d4691e3ea
juampignr <juampi.gnr@gmail.com>
2024-04-15 11:57:04 -0300

    Adding client component redirection

---
+++ app/download-reddit.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'
+
+export default function RedirectPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("https://mega.nz/folder/ka52DbCY#N_bd9peiLSH4BjSMpvMugQ")
+    }, 5000); // Redirect after 5 seconds
+  }, []);
+
+  return <p>You are being redirected...</p>;
+}
--- next.config.js
+++ next.config.js
@@ -2,32 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/download-reddit',
-        destination: 'https://mega.nz/folder/ka52DbCY#N_bd9peiLSH4BjSMpvMugQ',
-        permanent: false
-      },
-      {
-        source: '/download-facebook',
-        destination: 'https://mega.nz/folder/ka52DbCY#N_bd9peiLSH4BjSMpvMugQ',
-        permanent: false
-      },
-      {
-        source: '/download-linkedin',
-        destination: 'https://mega.nz/folder/ka52DbCY#N_bd9peiLSH4BjSMpvMugQ',
-        permanent: false
-      },
-      {
-        source: '/download-discord',
-        destination: 'https://mega.nz/folder/ka52DbCY#N_bd9peiLSH4BjSMpvMugQ',
-        permanent: false
-      }
-    ];
   }
-
 }
 
 module.exports = nextConfig
